37e387e8c240dd61e980b457888d9ba15e0c38c2
Oscar <oscarcookeabbott@gmail.com>
2023-01-09 21:59:44 +1100

    Add basic bevy plugins for testing

--- src/main.rs
+++ src/main.rs
@@ -1,39 +1,12 @@
-mod vector;
-mod tween;
-
-use std::fmt::Display;
-
-use vector::*;
-use tween::*;
+use bevy::prelude::*;
 
 fn main() {
-    let p1 = vector!(1.0, 2.0, 3.0);
-    let p2 = vector!(4.0, 5.0, 6.0);
-
-    println!("P1:        {}", p1);
-    println!("P2:        {}", p2);
-    println!("P1 == P2:  {}", p1 == p2);
-    println!("P1 + P2:   {}", p1 + p2);
-    println!("P1 + 1.0:  {}", p1 + 1.0);
-    println!("P1 - P2:   {}", p1 - p2);
-    println!("P1 - 1.0:  {}", p1 - 1.0);
-    println!("P1 * P2:   {}", p1 * p2);
-    println!("P1 * 2.0:  {}", p1 * 2.0);
-    println!("P1 / P2:   {}", p1 / p2);
-    println!("P1 / 2.0:  {}", p1 / 2.0);
-
-    println!("P1 . P2:   {}", Vector::dot(&p1, &p2));
-    println!("P1 x P2:   {}", Vector::cross(&p1, &p2));
-
-    let mut val: f64 = 0.0;
-    let mut tween = Tween::new(&mut val, 0.0, 1.0, 1.0, &print);
-    tween.start();
-
-    val = 0.0;
-
-    println!("test: {}", val);
+    App::new()
+        .add_plugins(DefaultPlugins)
+        .add_system(hello_world)
+        .run();
 }
 
-fn print<T: Display>(value: &T) {
-    println!("val: {}", value)
+fn hello_world() {
+    println!("hello world!");
 }
